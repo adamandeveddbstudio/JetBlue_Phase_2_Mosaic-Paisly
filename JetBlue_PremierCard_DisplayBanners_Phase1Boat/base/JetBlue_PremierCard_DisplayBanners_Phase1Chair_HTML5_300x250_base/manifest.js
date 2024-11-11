49453560fff94cda4c9a8f2290f849552427de14
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2024-11-11 18:39:59 +0530

    Add files via upload

--- JetBlue_PremierCard_DisplayBanners_Phase1Boat/base/JetBlue_PremierCard_DisplayBanners_Phase1Chair_HTML5_300x250_base/manifest.js
+++ JetBlue_PremierCard_DisplayBanners_Phase1Boat/base/JetBlue_PremierCard_DisplayBanners_Phase1Chair_HTML5_300x250_base/manifest.js
@@ -5,7 +5,7 @@ FT.manifest({
   "clickTagCount": 1,
   "hideBrowsers": ["ie8"],
   "richloads": [
-   {"name":"main_rl", "src":"JetBlue_PremierCard_DisplayBanners_Phase1Chair_HTML5_300x250"}
+   {"name":"main_rl", "src":"JetBlue_PremierCard_DisplayBanners_Phase1Boat_HTML5_300x250"}
  ],
   "instantAds": [
       {"name":"main_rl",          "type":"richload"}, 
